8f153864445a65e8687b214b71452ffebfb6acac
Ryan Norris <rnorris97@bertie.k12.nc.us>
2014-04-18 20:53:52 -0400

    Cayman is a device family therefore the Viper can't be called caymon.

--- device_cayman.mk
+++ device_cayman.mk
@@ -3,11 +3,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/lge/cayman/cayman-vendor.mk)
+$(call inherit-product-if-exists, vendor/lge/ls840/ls840-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/lge/cayman/overlay
 
-LOCAL_PATH := device/lge/cayman
+LOCAL_PATH := device/lge/ls840
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
@@ -23,7 +23,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_cayman
 PRODUCT_DEVICE := cayman
 PRODUCT_BRAND := lge
-PRODUCT_MODEL := cayman
+PRODUCT_MODEL := ls840
 PRODUCT_MANUFACTURER := lge
 
 # Recovery
